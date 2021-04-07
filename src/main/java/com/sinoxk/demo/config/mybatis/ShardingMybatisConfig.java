e4bd60b59132514eeb0872d6de04b87f1801cb6e
lizebin <lizebin0918@gmail.com>
2021-04-07 20:01:56 +0800

    先绕过报错，后续再找原因

--- src/main/java/com/sinoxk/demo/config/mybatis/ShardingMybatisConfig.java
+++ src/main/java/com/sinoxk/demo/config/mybatis/ShardingMybatisConfig.java
@@ -2,36 +2,14 @@ package com.sinoxk.demo.config.mybatis;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import com.sinoxk.demo.common.constant.Constant;
 import com.sinoxk.demo.config.interceptor.PageInterceptor;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
-import org.springframework.context.annotation.*;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
+@Configuration
 public class ShardingMybatisConfig {
-
-    @Resource(name = Constant.SHARDING_DATA_SOURCE)
-    private DataSource shardingDataSource;
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-        MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
-        factory.setDataSource(shardingDataSource);
-        factory.setTypeAliasesPackage("com.sinoxk.demo.dao.entity");
-        //添加XML目录
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        resolver.getResources("classpath:mapper/*.xml");
-        factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
-        return factory.getObject();
-    }
 
     @Bean
     public MapperScannerConfigurer mapperScannerConfigurer() {
